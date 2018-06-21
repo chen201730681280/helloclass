80d60f2dd17cd14422ac75a6fee7499315119aa6
Your Name <you@example.com>
2018-06-21 15:57:00 +0800

    1

---
+++ helloclass.cpp
@@ -0,0 +1,13 @@
+#include"helloclass.h"
+HelloClass::HelloClass()
+{
+cout<<"I am being created"<<endl;
+}
+HelloClass::~HelloClass()
+{
+cout<<"I am being destroyed"<<endl;
+}
+void HelloClass::sayHello()
+{
+cout<<"Hello Class!"<<endl;
+}
