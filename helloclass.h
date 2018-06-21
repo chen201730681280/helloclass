eff32c83aeb4377d7955f1da11b64d3fa66c6dbf
Your Name <you@example.com>
2018-06-21 15:56:40 +0800

    1

---
+++ helloclass.h
@@ -0,0 +1,14 @@
+fndef HELLOCLASS_H
+#define HELLOCLASS_H
+
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+class HelloClass
+{
+public:
+HelloClass();
+~HelloClass();
+void sayHello();
+};
+#endif
